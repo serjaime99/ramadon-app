c8b9978121c31fb4de86a4b0f61f6a7e52c40e24
serjaime99 <serjaime99@gmail.com>
2026-04-23 02:23:16 +0100

    map /recipes route to RecipeController

---
+++ resources/views/recipes/index.blade.php
@@ -0,0 +1,16 @@
+1 <!DOCTYPE html>
+2 <html>
+3 <head>
+4     <title>My Recipes</title>
+5 </head>
+6 <body>
+7     <h1>My Recipes</h1>
+8     
+9     @foreach($recipes as $recipe)
+10         <div>
+11             <h2>{{ $recipe->title }}</h2>
+12             <p>{{ $recipe->content }}</p>
+13         </div>
+14     @endforeach
+15 </body>
+16 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+route::get('/recipes', [RecipeController::class, 'index']);
