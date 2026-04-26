d6bce9c76b327802c862fee9acad732abd758b26
serjaime99 <serjaime99@gmail.com>
2026-04-26 02:06:23 +0100

    implement update and delete functionality

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -7,33 +7,52 @@ use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    /**
-     * display a listing of all recipes
-     */
-    public function index()
-    {
-        // 1. list all recipes
-        $recipes = Recipe::all();
-        // 2. sending the data to the view 
-        return view('recipes.index', compact('recipes'));
-    }
-    public function create()
-    {
-        return view('recipes.create');
-    }
+  /**
+   * display a listing of all recipes
+   */
+  public function index()
+  {
+    // 1. list all recipes
+    $recipes = Recipe::all();
+    // 2. sending the data to the view 
+    return view('recipes.index', compact('recipes'));
+  }
+  public function create()
+  {
+    return view('recipes.create');
+  }
 
-    public function store(Request $request)
-    {
-        // 1. Validation: Force the user to provide a title and content
-        $validated = $request->validate([
-            'title' => 'required',
-            'content' => 'required',
-        ]);
+  public function store(Request $request)
+  {
+    // 1. Validation: Force the user to provide a title and content
+    $validated = $request->validate([
+      'title' => 'required',
+      'content' => 'required',
+    ]);
 
-        // 2. Save to database using the Model
-        \App\Models\Recipe::create($validated);
+    // 2. Save to database using the Model
+    \App\Models\Recipe::create($validated);
 
-        // 3. Redirect the user back to the list so they can see their new recipe
-        return redirect('/recipes');
-    }
+    // 3. Redirect the user back to the list so they can see their new recipe
+    return redirect('/recipes');
+  }
+  // Show the form to edit
+  public function edit(Recipe $recipe)
+  {
+    return view('recipes.edit', compact('recipe'));
+  }
+
+  // Save the changes
+  public function update(Request $request, Recipe $recipe)
+  {
+    $recipe->update($request->validate(['title' => 'required', 'content' => 'required']));
+    return redirect('/recipes');
+  }
+
+  // Remove from database
+  public function destroy(Recipe $recipe)
+  {
+    $recipe->delete();
+    return redirect('/recipes');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\RecipeController;
+use App\Models\Recipe;
 use Illuminate\Support\Facades\Route;
 
 
 route::get('/recipes', [RecipeController::class, 'index']);
 Route::get('/recipes/create', [RecipeController::class, 'create']);
 Route::post('/recipes', [RecipeController::class, 'store']);
+route::get('/recipes/{recipe}/edit', [RecipeController::class, 'edit']);
+route::put('/recipes/{recipe}/update', [RecipeController::class, 'update']);
+Route::delete('/recipes/{recipe}', [RecipeController::class, 'destroy']);
